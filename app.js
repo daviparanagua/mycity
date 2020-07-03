9d023593a80b6ba8ec0e586c93929015257cf5c9
Davi Paranaguá Vital <daviparanagua@yahoo.com.br>
2020-07-03 09:25:32 -0300

    CORS enhancement

--- app.js
+++ app.js
@@ -9,7 +9,10 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-app.use(cors())
+app.use(cors({origin: true, credentials: true}))
+
+
+app.options('*', cors())
 
 app.use(logger('dev'));
 app.use(express.json());
