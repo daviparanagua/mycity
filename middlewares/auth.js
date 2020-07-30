09431c7ac109bd008a5c8848b70578e6c998c64f
Davi Paranaguá Vital <daviparanagua@yahoo.com.br>
2020-07-29 21:30:21 -0300

    Status code on expired token

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,7 +5,7 @@ function verifyJWT(req, res, next){
     if (!token) return res.status(401).json({ auth: false, message: 'No token provided.' });
     
     jwt.verify(token, process.env.JWT_SECRET, function(err, decoded) {
-      if (err) return res.status(500).json({ auth: false, message: 'Failed to authenticate token.' });
+      if (err) return res.status(401).json({ auth: false, message: 'Failed to authenticate token.' });
       
       // se tudo estiver ok, salva no request para uso posterior
       req.userId = decoded.id;
